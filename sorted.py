11569b7b3c4a9d026ce621ea4c90ef281a95daff
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2022-10-10 11:02:59 -0400

    Create sorted.py

---
+++ sorted.py
@@ -0,0 +1,16 @@
+## ACTIVITY: print SORTED list of CLA (Command Line Argument) words
+## NOTE: you should NOT need to write your own sorting algorithm
+
+## USAGE: python sorted.py bears BEETS battlestar Galactica
+## OUTPUT: battlestar bears BEETS Galactica
+
+## USAGE: python sorted.py b3ars BEETS b@ttlestar Galactica
+## OUTPUT: "Usage: Send only letters!!""
+
+## IMPORT: functions argv for CLAs && exit for exit without main
+from sys import argv, exit
+argv(1)
+
+# TODO: SORT LIST OF CLAs
+
+# TODO: PRINT USAGE NOTICE IF ANY WORDS CONTAIN NON-ALPHA CHARACTERS, ELSE PRINT SORTED LIST
